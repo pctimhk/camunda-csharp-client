5aab4f46d2faf7d16bfaf9788fb879370a261eb0
tt1366 <yim.tim@bcthk.com>
2021-11-18 18:11:58 +0800

    Develop unit test for the camundaEngineClient

--- CamundaClientLibrary.Test/CamundaEngineClientTest.cs
+++ CamundaClientLibrary.Test/CamundaEngineClientTest.cs
@@ -12,7 +12,7 @@ namespace CamundaClientLibrary.Test
         [SetUp]
         public void Setup()
         {
-            camundaEngineClient = new CamundaEngineClient(new System.Uri(@"http://192.168.17.158:29090//engine-rest/engine/default/"), null, null, System.Reflection.Assembly.GetExecutingAssembly());
+            camundaEngineClient = new CamundaEngineClient(new System.Uri(@"http://192.168.17.158:29090/engine-rest/engine/default/"), null, null, System.Reflection.Assembly.GetExecutingAssembly());
 
             // deploy BPMN
             //camundaEngineClient.RepositoryService.Deploy(@"CamundaEngineLibraryTest", @"BPMN\CamundaEngineLibraryTest.bpmn");
@@ -22,6 +22,13 @@ namespace CamundaClientLibrary.Test
         public void TestStartupWithSingleThreadPolling()
         {
             camundaEngineClient.StartupWithSingleThreadPolling();
+            while (System.Diagnostics.Debugger.IsAttached)
+            {
+                // code or timeout value when running tests in debug mode
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            System.Threading.Thread.Sleep(30000);
         }
     }
 }
